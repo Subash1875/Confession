90bbd1e7735b3d774ef25e24fe21acae80c805f4
Subash1875 <subashk72005@gmail.com>
2025-08-12 15:06:22 +0530

    change : database from sqlite to mysql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'rest_framework'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'confession',
+        'USER' : 'root',
+        'PASSWORD' : 'Subashk@18072005',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
     }
 }
 
