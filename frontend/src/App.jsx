b4b9584f2215426ffdabadd764820ad5b03dc0ac
Subash1875 <subashk72005@gmail.com>
2025-08-12 11:40:04 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./static/css/style.css";
+
+function App() {
+  return <>
+    <h1 className="text-center">hello world!</h1>
+  </>
+}
+
+export default App;
